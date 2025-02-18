8b4b56732d2a6a71f98a8f05c23d0a2882bc38b2
YuYeaJin <yyj9290@naver.com>
2025-02-18 18:11:37 +0900

    [예진] 스프링부트 연결 설정

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import JejuMap from "./components/maps/JejuMap";
 import LoginPage from "./components/contents/LoginPage";
 
 function App() {
+  const [message, setMessage] = useState("서버 응답 대기 중...");
+
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(response => setMessage(JSON.stringify(response.data))) // 데이터를 JSON 형태로 변환해서 출력
+      .catch(error => setMessage(`에러 발생: ${error.message}`));
+    }, []);
+
   return (
     <Router>
       <MenuHeader />
@@ -31,8 +39,15 @@ function App() {
         />
       </Routes>
       <Footer />
+
+        {/* 스프링부트 API 연결 확인 */}
+        <div style={{ padding: "20px", backgroundColor: "#f8f9fa", textAlign: "center" }}>
+        <h2>Spring Boot API 연결 테스트</h2>
+        <p>{message}</p>
+      </div>
     </Router>
   );
+
 }
 
 export default App;
