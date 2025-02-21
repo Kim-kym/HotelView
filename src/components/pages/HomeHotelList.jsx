e32a25ae90423f5cef383bf9785a86745fcbd022
YuYeaJin <yyj9290@naver.com>
2025-02-21 11:15:25 +0900

    [예진] 호텔 카드 전체 링크화 및 호버 추가

--- src/components/pages/HomeHotelList.jsx
+++ src/components/pages/HomeHotelList.jsx
@@ -9,14 +9,6 @@ function HotelListDummy() {
   console.log("더미 호텔 데이터:", dummyHotels); // ✅ 콘솔에서 확인
   console.log("현재 상태 값:", hotels); // ✅ 상태값 확인
 
-  // useEffect(() => {
-  //   if (!dummyHotels || dummyHotels.length === 0) {
-  //     console.error("더미 호텔 데이터가 비어 있음!");
-  //   } else {
-  //     setHotels(dummyHotels);
-  //   }
-  // }, []);
-
   useEffect(() => {
     setHotels(dummyHotels);
   }, []);
@@ -26,15 +18,17 @@ function HotelListDummy() {
       <h2>호텔</h2>
       <div className="hotel-list">
         {hotels.map((hotel) => (
-          <div key={hotel.id} className="hotel-box">
-            <img src={hotel.image} alt={hotel.name} className="hotel-image" />
-            <Link to={`/hotels/${hotel.id}`}>
-              <h3>{hotel.name}</h3>
-              <p>{hotel.address}</p>
-              <p>⭐ {hotel.rating}</p>
-              <p>₩ {hotel.price}</p>
-            </Link>
-          </div>
+          <Link to={`/hotels/${hotel.id}`} key={hotel.id} className="hotel-box-link">
+            <div className="hotel-box">
+              <img src={hotel.image} alt={hotel.name} className="hotel-image" />
+              <div className="hotel-info">
+                <h3>{hotel.name}</h3>
+                <p>{hotel.address}</p>
+                <p>⭐ {hotel.rating}</p>
+                <p>₩ {hotel.price}</p>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
