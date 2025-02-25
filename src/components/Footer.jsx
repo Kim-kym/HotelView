f3731165383bca2e2d2ace77b0e01f2c57402721
junil3005 <yjl3005@naver.com>
2025-02-25 10:17:22 +0900

    [준일] 푸터 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import "../styled/Footer.css";
 function Footer() {
   return (
     <footer>
+      <p>© 2024 자쿠과. All rights reserved.</p>
       <p>@자쿠과</p>
       <p>연락처: 000-0000-0000</p>
     </footer>
