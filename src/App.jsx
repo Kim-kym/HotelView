4222784cf4c953ed1d7638207ff72c0205329d03
junil3005 <yjl3005@naver.com>
2025-02-25 17:30:16 +0900

    [준일] 결제창 추가 및 사진 첨가 qr코드

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-// import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./components/contexts/AuthContext";
 import MenuHeader from "./components/MenuHeader";
 import Footer from "./components/Footer";
 import CompanyIntro from "./components/pages/CompanyIntro";
-// 소개 추가
 import SignInPage from "./components/pages/SignInPage";
 import SignUpPage from "./components/pages/SignUpPage";
-// import HotelList from "./components/pages/HotelList";
 import HotelListDummy from "./components/pages/HotelListDummy";
 import MyPage from "./components/pages/MyPage";
 import Admin from "./components/pages/Admin";
@@ -17,8 +14,9 @@ import HotelReserve from "./components/pages/HotelReserve";
 import PasswordCheck from "./components/pages/PasswordCheck";
 import EditProfile from "./components/pages/EditProfile";
 import ReservationConfirm from "./components/pages/ReservationConfirm";
-import { useEffect } from "react";
 import Board from "./components/pages/Board";
+import PaymentModalManager from "./components/forms/PaymentModalManager"; // PaymentModalManager 추가
+import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
@@ -46,25 +44,27 @@ function App() {
         <div className="fixed-header">
           <MenuHeader />
         </div>
-        <Routes>
-          <Route path="/" element={<MapWithSearch />} />
-          <Route path="/login" element={<SignInPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/mypage/password-check" element={<PasswordCheck />} />
-          <Route path="/mypage/edit" element={<EditProfile />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          {/* <Route path="/hotels" element={<HotelList />} /> */}
-          <Route path="/board" element={<Board />} />
-          <Route path="/hotels" element={<HotelListDummy />} />
-          <Route path="/hotels/:id" element={<HotelDetail />} />
-          <Route path="/reservation/:id" element={<HotelReserve />} />
-          <Route path="/reservationConfirm" element={<ReservationConfirm />} /> 
-          {/* 회원가입 페이지 */}
-          <Route path="/reserve" element={<MapWithSearch />} />
-          <Route path="/companyIntro" element={<CompanyIntro />} />
-        </Routes>
+        <div className="content-wrapper">
+          <Routes>
+            <Route path="/" element={<MapWithSearch />} />
+            <Route path="/login" element={<SignInPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/mypage/password-check" element={<PasswordCheck />} />
+            <Route path="/mypage/edit" element={<EditProfile />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="/hotels" element={<HotelListDummy />} />
+            <Route path="/hotels/:id" element={<HotelDetail />} />
+            <Route path="/reservation/:id" element={<HotelReserve />} />
+            <Route path="/reservationConfirm" element={<ReservationConfirm />} />
+            <Route path="/reserve" element={<MapWithSearch />} />
+            <Route path="/companyIntro" element={<CompanyIntro />} />
+          </Routes>
+        </div>
         <Footer />
+        {/* PaymentModalManager를 항상 렌더링하여 모달 전환을 관리 */}
+        <PaymentModalManager />
       </Router>
     </AuthProvider>
   );
