e34bae5ce86f154ac8e1afba530fe0bb1e079c24
Kim-kym <msgim725@gmail.com>
2025-02-25 12:10:19 +0900

    [현석] springboot api연결 초기작업

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {  // 🔥 '/api'로 시작하는 요청을 자동으로 변환
+        target: 'http://localhost:8050',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '') // '/api' 제거 후 요청 전송
+      }
+    }
+  }
+});
